43ba5268ba1f8703161e0b5623fb8583b8d8447e
Azam Drus <nrazam95@gmail.com>
2023-12-25 01:10:27 +0800

    flutter sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,15 +44,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,70 +51,140 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FlutterSoundRecorder recorder = FlutterSoundRecorder();
+  AudioPlayer player = AudioPlayer();
+  bool isRecorderReady = false;
+  bool isPlayerReady = false;
+  String? _path;
+  bool isRecording = false;
+  bool isPlaying = false;
 
-  void _incrementCounter() {
+  Future<void> record() async {
+    if (!isRecorderReady) {
+      return;
+    }
+    await recorder.startRecorder(
+      toFile: 'audio_example.mp4',
+      codec: Codec.aacMP4,
+    );
+  }
+
+  Future<void> stop() async {
+    if (!recorder.isRecording) {
+      return;
+    }
+    final path = await recorder.stopRecorder();
+    print(path);
+    final audioFile = File(path!);
+
+    if (audioFile.existsSync()) {
+      print('File exists');
+      isPlayerReady = true;
+
+      _path = audioFile.path;
+    } else {
+      print('File does not exist');
+      isPlayerReady = false;
+    }
+  }
+
+  Future<void> play() async {
+    await player.play(UrlSource(_path!));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isPlaying = true;
+    });
+  }
+
+  Future<void> stopPlayer() async {
+    await player.stop();
+    setState(() {
+      isPlaying = false;
     });
   }
 
   @override
+  void initState() {
+    initRecorder();
+    // initPlayer();
+    super.initState();
+  }
+
+  Future<void> initRecorder() async {
+    final status = await Permission.microphone.request();
+
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException('Microphone permission not granted');
+    }
+
+    await recorder.openRecorder();
+
+    setState(() {
+      isRecorderReady = true;
+    });
+    recorder.setSubscriptionDuration(const Duration(milliseconds: 1000));
+  }
+
+  // Future<void> initPlayer() async {
+  //   await player.openPlayer();
+  // }
+
+  @override
+  void dispose() {
+    recorder.closeRecorder();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+        // Create a Recorder
+        child: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            isPlayerReady
+                ? ElevatedButton(
+                    onPressed: () async {
+                      if (isPlaying) {
+                        await stopPlayer();
+                      } else {
+                        await play();
+                      }
+                    },
+                    child: Icon(
+                      // ignore: dead_code
+                      isPlaying ? Icons.stop : Icons.mic,
+                      size: 50,
+                    ))
+                : Container(),
+            StreamBuilder<RecordingDisposition>(
+                stream: recorder.onProgress,
+                builder: (context, snapshot) {
+                  final duration = snapshot.data?.duration ?? Duration.zero;
+
+                  return Text('${duration.inSeconds} seconds');
+                }),
+            const SizedBox(
+              height: 20,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ElevatedButton(
+                onPressed: () async {
+                  if (recorder.isRecording) {
+                    await stop();
+                    isRecording = false;
+                  } else {
+                    await record();
+                    isRecording = true;
+                  }
+                },
+                child: Icon(
+                  // ignore: dead_code
+                  isRecording ? Icons.stop : Icons.mic,
+                  size: 50,
+                ))
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        )),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
